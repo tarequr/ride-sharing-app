26c597693aea0cebb7671ee7539795e6cdca94f3
simply-samin <samiulislamsamin7@gmail.com>
2024-02-11 22:53:21 +0600

    redirect to index page after login complete

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,24 +1,60 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router'
 // import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/LoginView.vue';
+import LoginView from '../views/LoginView.vue'
+import LandingView from '../views/LandingView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'login',
-      component: LoginView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'login',
+            component: LoginView,
+        },
+        {
+            path: '/landing',
+            name: 'landing',
+            component: LandingView,
+        },
+        {
+            path: '/about',
+            name: 'about',
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import('../views/AboutView.vue'),
+        },
+    ],
 })
+
+router.beforeEach((to, from) => {
+    if (to.name === 'login') {
+        return true
+    }
+
+    if (!localStorage.getItem('token')) {
+        return {
+            name: 'login',
+        }
+    }
+
+    checkTokenAuthenticity()
+})
+
+const checkTokenAuthenticity = () => {
+    axios
+        .get('http://localhost/api/user', {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+            },
+        })
+        .then((response) => {})
+        .catch((error) => {
+            localStorage.removeItem('token')
+            router.push({
+                name: 'login',
+            })
+        })
+}
 
 export default router
---
+++ frontend/src/views/LandingView.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="pt-16">
+        <h1 class="text-3xl font-semibold mb-4">Ride Share</h1>
+        <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
+            <div class="bg-white px-4 py-5 sm:p-6">
+                <div class="flex justify-between">
+                    <button
+                        class="rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">
+                        Start Driving
+                    </button>
+                    <button
+                        class="rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">
+                        Find A Ride
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
