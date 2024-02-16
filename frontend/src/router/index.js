b14df8444dd53c2032edacc0ba306da786768204
simply-samin <samiulislamsamin7@gmail.com>
2024-02-17 02:35:59 +0600

    added vue google maps package and also Map view

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import LandingView from '../views/LandingView.vue'
 import LocationView from '../views/LocationView.vue'
+import MapView from '../views/MapView.vue'
 import axios from 'axios'
 
 const router = createRouter({
@@ -30,6 +31,11 @@ const router = createRouter({
             path: '/location',
             name: 'location',
             component: LocationView
+        },
+        {
+            path: '/map',
+            name: 'map',
+            component: MapView
         }
     ],
 })
---
+++ frontend/src/views/MapView.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+import { useLocationStore } from '@/stores/location'   
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+const location = useLocationStore()
+
+</script>
+
+<template>
+    <div class="pt-16">
+        <h1 class="text-3xl font-semibold mb-4">Here's your trip</h1>
+        <div>
+            <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
+                <div class="bg-white px-4 py-5 sm:p-6">
+                    <div>
+                        <!-- <GMapMap v-if="location.destination.name !== ''" :zoom="11" :center="location.destination.geometry"
+                            ref="gMap"
+                            style="width: 100%; height: 256px;">
+                        </GMapMap> -->
+                    </div>
+                    <div class="mt-2">
+                        <p class="text-xl">Going to <strong>{{ location.destination.name }}</strong></p>
+                    </div>
+                </div>
+                <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                    <button
+                        class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none"
+                    >
+                        Let's Go!
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
